0db823c2d98e8d6ad94486863bbc747d8fba7c5e
Степан Дворников <stepandv@Stepans-MacBook-Air.local>
2024-12-06 15:59:32 +0300

    Пофиксил баг с рестартом и внес мелкие коррективы в интерфейс игры

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ class Maze:
                 self.maze[i][j] = random.randint(0, 1)
 
     def draw(self, frame, ox, oy):
+        global game_started
+        cv2.putText(frame, "Keep your hand 2-3 meters away from the camera",
+                    (window_width // 2 - 400, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
         for i in range(-1, self.rows + 1):
             for j in range(-1, self.cols + 1):
                 x1, y1 = ox + j * self.cell_width, oy + i * self.cell_height
@@ -27,8 +31,10 @@ class Maze:
                 if i == -1 or j == -1 or i == self.rows or j == self.cols or self.maze[i][j] == 1:
                     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), -1)
 
-        cv2.circle(frame, (ox + self.cell_width // 2, oy + self.cell_height // 2), min(self.cell_width, self.cell_height) // 4,
-                   (0, 255, 0), -1)
+        if not game_started:
+            cv2.circle(frame, (ox + self.cell_width // 2, oy + self.cell_height // 2), min(self.cell_width, self.cell_height) // 4,
+                       (0, 255, 0), -1)
+
         cv2.circle(frame, (ox + (self.cols - 1) * self.cell_width + self.cell_width // 2,
                            oy + (self.rows - 1) * self.cell_height + self.cell_height // 2),
                    min(self.cell_width, self.cell_height) // 4, (255, 0, 0), -1)
@@ -56,92 +62,105 @@ class Maze:
 
         return False
 
+def set_settings():
+    global maze, game_started, victory, game_over, rows, cols
+    maze.generate()
 
-def main():
-    cap = cv2.VideoCapture(1)
-    window_width = 1000
-    window_height = 600
-    maze_width = 500
-    maze_height = 350
-    rows = 8
-    cols = 10
-
-    while cap.isOpened():
-        maze = Maze(maze_width, maze_height, rows, cols)
+    while not maze.solve((0, 0), (rows - 1, cols - 1)):
         maze.generate()
 
-        while not maze.solve((0, 0), (rows - 1, cols - 1)):
-            maze.generate()
-
-        handsDetector = mp.solutions.hands.Hands()
-
-        game_started = False
-        victory = False
-        game_over = False
-
-        ox = (window_width - maze_width) // 2
-        oy = (window_height - maze_height) // 2
-
-        while cap.isOpened():
-            ret, frame = cap.read()
-
-            if cv2.waitKey(1) & 0xFF == 27 or not ret:
-                exit(0)
-
-            if cv2.waitKey(1) & 0xFF == ord('r'):
-                break
-
-            frame = cv2.flip(frame, 1)
-
-            frame = cv2.resize(frame, (window_width, window_height))
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            results = handsDetector.process(frame)
-
-            if results.multi_hand_landmarks and not victory:
-                x_tip = int(results.multi_hand_landmarks[0].landmark[8].x *
-                            frame.shape[1])
-                y_tip = int(results.multi_hand_landmarks[0].landmark[8].y *
-                            frame.shape[0])
-                cv2.circle(frame, (x_tip, y_tip), 10, (255, 0, 0), -1)
-
-                finish_x = ox + (cols - 1) * maze.cell_width + maze.cell_width // 2
-                finish_y = oy + (rows - 1) * maze.cell_height + maze.cell_height // 2
-                start_x = ox + maze.cell_width // 2
-                start_y = oy + maze.cell_height // 2
-
-                if game_started and not victory:
-                    maze_x = (x_tip - ox) // maze.cell_width
-                    maze_y = (y_tip - oy) // maze.cell_height
-                    if maze_x == -1 or maze_x == maze.cols or maze_y == -1 or maze_y == maze.rows or (
-                            0 <= maze_x < cols and 0 <= maze_y < rows and maze.maze[maze_y][maze_x] == 1):
-                        game_over = True
-
-                if not game_started and abs(x_tip - start_x) < maze.cell_width // 2 and abs(
-                        y_tip - start_y) < maze.cell_height // 2:
-                    game_started = True
-
-                if not game_over and game_started and (x_tip - finish_x) < maze.cell_width // 2 and abs(
-                        y_tip - finish_y) < maze.cell_height // 2:
-                    victory = True
-
-            maze.draw(frame, ox, oy)
-
-            if not game_started and not game_over:
-                cv2.putText(frame, "Place your finger in the START circle!",
-                            (window_width // 2 - 300, window_height - 50),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
-
-            if victory:
-                cv2.putText(frame, "YOU WIN! Press R to restart the game",
-                            (window_width // 2 - 300, window_height - 50),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
-
-            if game_over:
-                cv2.putText(frame, "Game Over! Press R to restart the game",
-                            (window_width // 2 - 350, window_height - 50),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-
-            cv2.imshow("Maze Game", frame)
+    game_started = False
+    victory = False
+    game_over = False
 
 
-main()
+cap = cv2.VideoCapture(1)
+window_width = 1000
+window_height = 600
+maze_width = 500
+maze_height = 350
+rows = 8
+cols = 10
+
+maze = Maze(maze_width, maze_height, rows, cols)
+
+ox = (window_width - maze_width) // 2
+oy = (window_height - maze_height) // 2
+
+handsDetector = mp.solutions.hands.Hands()
+
+game_started = False
+victory = False
+game_over = False
+
+set_settings()
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == 27:
+        exit(0)
+
+    if key == 13:
+        set_settings()
+
+    frame = cv2.flip(frame, 1)
+
+    frame = cv2.resize(frame, (window_width, window_height))
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = handsDetector.process(frame)
+
+    if results.multi_hand_landmarks and not victory:
+        x_tip = int(results.multi_hand_landmarks[0].landmark[8].x *
+                    frame.shape[1])
+        y_tip = int(results.multi_hand_landmarks[0].landmark[8].y *
+                    frame.shape[0])
+
+        if not game_started:
+            cv2.circle(frame, (x_tip, y_tip), 10, (255, 0, 0), -1)
+        else:
+            cv2.circle(frame, (x_tip, y_tip), 10, (0, 255, 0), -1)
+
+        finish_x = ox + (cols - 1) * maze.cell_width + maze.cell_width // 2
+        finish_y = oy + (rows - 1) * maze.cell_height + maze.cell_height // 2
+        start_x = ox + maze.cell_width // 2
+        start_y = oy + maze.cell_height // 2
+
+        if game_started and not victory:
+            maze_x = (x_tip - ox) // maze.cell_width
+            maze_y = (y_tip - oy) // maze.cell_height
+            if maze_x == -1 or maze_x == maze.cols or maze_y == -1 or maze_y == maze.rows or (
+                    0 <= maze_x < cols and 0 <= maze_y < rows and maze.maze[maze_y][maze_x] == 1):
+                game_over = True
+
+        if not game_started and abs(x_tip - start_x) < maze.cell_width // 2 and abs(
+                y_tip - start_y) < maze.cell_height // 2:
+            game_started = True
+
+        if not game_over and game_started and (x_tip - finish_x) < maze.cell_width // 2 and abs(
+                y_tip - finish_y) < maze.cell_height // 2:
+            victory = True
+
+    maze.draw(frame, ox, oy)
+
+    if not game_started and not game_over:
+        cv2.putText(frame, "Place your finger in the START circle!",
+                    (window_width // 2 - 300, window_height - 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+
+    if victory:
+        cv2.putText(frame, "YOU WIN! Press ENTER to restart the game",
+                    (window_width // 2 - 320, window_height - 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+
+    if game_over:
+        cv2.putText(frame, "Game Over! Press ENTER to restart the game",
+                    (window_width // 2 - 350, window_height - 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+
+    cv2.imshow("Maze Game", frame)
